91f25f6728d8f25ecdbd23e72473a448365b4481
vlad <vklokun@protonmail.ch>
2018-10-29 02:10:52 +0200

    Y03S01: System Software: Lab 05: add solution and report

---
+++ y03s01/system-software/lab-05/02-report/assets/Driver.c
@@ -0,0 +1,82 @@
+#include "driver.h"
+#include "driver.tmh"
+
+#ifdef ALLOC_PRAGMA
+#pragma alloc_text (INIT, DriverEntry)
+#pragma alloc_text (PAGE, y03s01syssoftlab05EvtDeviceAdd)
+#pragma alloc_text (PAGE, y03s01syssoftlab05EvtDriverContextCleanup)
+#endif
+
+NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT  DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+    )
+{
+    WDF_DRIVER_CONFIG config;
+    NTSTATUS status;
+    WDF_OBJECT_ATTRIBUTES attributes;
+
+    WPP_INIT_TRACING(DriverObject, RegistryPath);
+
+    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
+
+    WDF_OBJECT_ATTRIBUTES_INIT(&attributes);
+    attributes.EvtCleanupCallback = y03s01syssoftlab05EvtDriverContextCleanup;
+
+    WDF_DRIVER_CONFIG_INIT(&config,
+                           y03s01syssoftlab05EvtDeviceAdd
+                           );
+
+    status = WdfDriverCreate(DriverObject,
+                             RegistryPath,
+                             &attributes,
+                             &config,
+                             WDF_NO_HANDLE
+                             );
+
+    if (!NT_SUCCESS(status)) {
+        TraceEvents(TRACE_LEVEL_ERROR, TRACE_DRIVER, "WdfDriverCreate failed %!STATUS!", status);
+        WPP_CLEANUP(DriverObject);
+        return status;
+    }
+
+    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Exit");
+
+    return status;
+}
+
+NTSTATUS
+y03s01syssoftlab05EvtDeviceAdd(
+    _In_    WDFDRIVER       Driver,
+    _Inout_ PWDFDEVICE_INIT DeviceInit
+    )
+{
+    NTSTATUS status;
+
+    UNREFERENCED_PARAMETER(Driver);
+
+    PAGED_CODE();
+
+    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
+
+    status = y03s01syssoftlab05CreateDevice(DeviceInit);
+
+    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Exit");
+
+    return status;
+}
+
+VOID
+y03s01syssoftlab05EvtDriverContextCleanup(
+    _In_ WDFOBJECT DriverObject
+    )
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+
+    PAGED_CODE();
+
+    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
+
+    WPP_CLEANUP(WdfDriverWdmGetDriverObject((WDFDRIVER)DriverObject));
+}
