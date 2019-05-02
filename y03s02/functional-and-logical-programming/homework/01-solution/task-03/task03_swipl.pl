0ff1062fa9511bad66d734cc396493d050fd5486
vlad <vklokun@protonmail.ch>
2019-05-02 12:27:52 +0300

    Y03S02: Functional and Logical Programming: Homework 01: Task 03: modify subtask 01 and complete subtask 02

--- y03s02/functional-and-logical-programming/homework/01-solution/task-03/task03_swipl.pl
+++ y03s02/functional-and-logical-programming/homework/01-solution/task-03/task03_swipl.pl
@@ -1,42 +1,43 @@
 % Solution for SWI Prolog
-% moto(Vendor, Type, Engine_Capacity, RPM, Transmisison, Color, MPG, Price).
-% moto(Vendor, Type, Engine_Capacity, RPM, FuelType, Price)
-moto(honda, scooter, 2000, 12000, gasoline, 10000).
-moto(yamaha, scooter, 2000, 12000, gasoline, 10000).
-moto(suzuki, bike, 2000, 12000, gasoline, 10000).
+% moto(
+%	1. Name,
+%	2. Type,
+%	3. Displacement --- volume of the engine;
+%	4. RPM_max,
+%	5. Transmisison type;
+%	6. Color,
+%	7. Fuel Consumption --- in L/100 km.
+%	8. Price
+% ).
+% moto(Name, Type, Displacement, RPM, FuelType, Price)
+moto(honda, scooter, 700, 12000, auto, black, 6, 10000).
+moto(yamaha, scooter, 600, 12000, manual, black, 8, 10000).
+moto(ducati, scooter, 800, 12000, auto, white, 7, 10000).
+moto(suzuki, bike, 1500, 12000, auto, black, 10, 10000).
 
 % client(
 % 	Name, Type, EC_min, EC_max, RPM_min, RPM_max, tran_type, color,
 % 	fuel_cons_min, fuel_cons_max, price
 % )
-client(john, scooter, 0, 0, 0, 0).
-client(mark, bike, 0, 0, 0, 0).
-client(mark, bike, 0, 0, 0, 0).
-client(mark, bike, 0, 0, 0, 0).
-client(mark, bike, 0, 0, 0, 0).
-client(mark, bike, 0, 0, 0, 0).
-client(clark, atv, 0, 0, 0, 0).
-client(clark, atv, 0, 0, 0, 0).
-client(clark, atv, 0, 0, 0, 0).
-client(kent, scooter, 0, 0, 0, 0).
-client(kent, scooter, 0, 0, 0, 0).
-client(kent, scooter, 0, 0, 0, 0).
-client(sid, bike, 0, 0, 0, 0).
-
-has_type(X, Type) :-
-	moto(X, Type, _, _, _, _).
+client(john, scooter, 20, 60, 6000, 12000, auto, black, 3, 7, 6000).
+client(mark, bike, 20, 60, 6000, 12000, auto, black, 3, 7, 6000).
+client(kirk, atv, 20, 60, 6000, 12000, auto, black, 3, 7, 6000).
+client(kent, scooter, 20, 60, 6000, 12000, auto, black, 3, 7, 6000).
+client(jeff, bike, 20, 60, 6000, 12000, auto, black, 3, 7, 6000).
 
 wants_type(Client, Type) :-
 % Checks if client Client wants type Type
-	client(Client, Type, _, _, _, _).
+	client(Client, Type, _, _, _, _, _, _, _, _, _).
 
 count_type_preferences(Type, C) :-
 % Counts how many clients want type Type
+% When Type is wanted C times
+	% Find all solutions where someone wants a type Type
 	findall(_, wants_type(_, Type), Z),
+	% An count them
 	length(Z, C).
 
 append_scooter_count(L, L2) :-
-	% Double square brackets to append as list
 	count_type_preferences(scooter, C),
 	append(L, [C-scooter], L2).
 
@@ -53,24 +54,36 @@ count_all_type_prefs(L, RL) :-
 	append_bike_count(L1, L2),
 	append_atv_count(L2, RL).
 
-% Define comparators for [Type, Count] pairs to use with predsort/2
-% type_count_cmp(>, [_, Count1], [_, Count2]) :-
-%	Count1 > Count2.
-
-%type_count_cmp(<, [_, Count1], [_, Count2]) :-
-%	Count1 < Count2.
-
-%type_count_cmp(=, [_, Count1], [_, Count2]) :-
-%	Count1 = Count2.
-%
-
-%sort_type_prefs(L, RL) :-
-%	predsort(type_count_cmp, L, RL).
-
 get_sorted_type_prefs(L, RL) :-
+% Sorts a list of pairs of form Count - Type by its key value (Count) in
+% descending order
+	% Count how many times a particular type of a moto vehicle is wanted
+	% and put the results in list L1
 	count_all_type_prefs(L, L1),
+	% Sort the preference pairs by their keys --- count of how many times
+	% they are wanted
 	keysort(L1, L2),
+	% Reverse the sort so the list is sorted in descending order
+	% This allows to get the top item as a list's Head
 	reverse(L2, RL).
 
-% Figure out how to get a head of passed list
-get_most_popular_pref([H | _], H).
+get_most_popular_pref(H) :-
+	get_sorted_type_prefs([], [H | _]).
+% Gets the top item in list --- its head. Assumes a sorted list
+
+% Task 2: print moto vehicles sorted by their engine capacity
+get_moto_ec(L) :-
+% Finds all pairs of vehicles Name with their corresponding Engine Capacity
+% and puts them into a list L
+	findall(EC-Name, moto(Name, _, EC, _, _, _, _, _), L).
+
+sort_moto_by_ec(RL) :-
+% Sorts a list of pairs of form Engine Capacity - Name by its key value ---
+% Engine Capacity in ascending order
+	get_moto_ec(L),
+	keysort(L, RL).
+
+% Task 3: count how many offers fit a client Client's wishes
+% TODO
+matches_wishes(C) :-
+	!.
